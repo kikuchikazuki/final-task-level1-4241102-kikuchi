bf2fbd02afd16ef52dcfe7177a79e3b58e67255c
和希 <k.kikuchi.sys24@morijyobi.ac.jp>
2026-01-27 16:22:35 +0900

    layoutの修正がひとまず終わりました。

--- common.js
+++ common.js
@@ -1,22 +1,43 @@
 "use strict";
 
 window.addEventListener("scroll", function () {
-    const btn = document.getElementById("backToTopBtn");
-    if (window.scrollY > 150) {
-        btn.style.display = "flex";
-        btn.style.opacity = "1";
-    } else {
-        btn.style.opacity = "0";
-        setTimeout(() => {
-            if (window.scrollY < 150) btn.style.display = "none";
-        }, 200);
-    }
+  const btn = document.getElementById("backToTopBtn");
+  if (window.scrollY > 150) {
+    btn.style.display = "flex";
+    btn.style.opacity = "1";
+  } else {
+    btn.style.opacity = "0";
+    setTimeout(() => {
+      if (window.scrollY < 150) btn.style.display = "none";
+    }, 200);
+  }
 });
 
 document.getElementById("backToTopBtn").addEventListener("click", function () {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    });
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
 });
+
+// ここから下は自分が変更した☟ 
+document.addEventListener("DOMContentLoaded", () => {
+  const genreButtons = document.querySelectorAll(".genre-btn");
+  const cards = document.querySelectorAll(".anime-card");
+
+  genreButtons.forEach(btn => {
+    btn.addEventListener("click", () => {
+      const genre = btn.dataset.genre;
+
+      cards.forEach(card => {
+        if (card.dataset.genre === genre) {
+          card.classList.remove("d-none");
+        } else {
+          card.classList.add("d-none");
+        }
+      });
+    });
+  });
+});
+
 
